703317c17d2d831ec5406f3cc9268a16ae0c9961
Mike Bijon <mikebijon@gmail.com>
2013-09-03 13:56:25 -0700

    Bases 1.0.0 - Starter files exempted from .gitignore

---
+++ wp-config.php
@@ -0,0 +1,81 @@
+<?php
+// ===================================================
+// Load database info and local development parameters
+// ===================================================
+if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+	
+	define( 'WP_LOCAL_DEV', true );
+	include( dirname( __FILE__ ) . '/local-config.php' );
+	
+	// ===========
+	// Show errors
+	// ===========
+	ini_set( 'display_errors', 1 );
+	define( 'WP_DEBUG_DISPLAY', true );
+	define( 'WP_DEBUG', true );
+	// define( 'SAVEQUERIES', true ); // Performance hit if enabled
+	
+	// ========================
+	// Custom Content Directory
+	// ========================
+	//define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/html/wp-content' );
+	//define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
+	
+} else {
+	
+	define( 'WP_LOCAL_DEV', false );
+	
+	define( 'DB_NAME', '%%DB_NAME%%' );
+	define( 'DB_USER', '%%DB_USER%%' );
+	define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
+	define( 'DB_HOST', '%%DB_HOST%%' );
+	
+	// ===========
+	// Hide errors
+	// ===========
+	ini_set( 'display_errors', 0 );
+	define( 'WP_DEBUG_DISPLAY', false );
+}
+
+// ================================================
+// You almost certainly do not want to change these
+// ================================================
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+// ==============================================================
+// Salts, for security
+// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
+// ==============================================================
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+// ==============================================================
+// Table prefix
+// Change this if you have multiple installs in the same database
+// ==============================================================
+$table_prefix  = 'wp_';
+
+// ================================
+// Language
+// Leave blank for American English
+// ================================
+define( 'WPLANG', '' );
+
+// ======================================
+// Load a Memcached config if we have one
+// ======================================
+if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
+	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
+
+// ===========================================================================================
+// This can be used to programatically set the stage when deploying (e.g. production, staging)
+// ===========================================================================================
+define( 'WP_STAGE', '%%WP_STAGE%%' );
+define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
